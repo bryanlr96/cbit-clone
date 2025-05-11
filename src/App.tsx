4c762c89ebc07240cd2839716f48135f6da931cf
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-11 22:07:50 +0200

    arreglando nacbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";  // Asegúrate de importar Layout
 import Playground from "./pages/Playground";
+import WorkInProgress from "./pages/WorkInProgress";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}> 
         <Route index element={<Playground />} /> 
+        <Route path="/activity" element={<WorkInProgress />} /> 
+        <Route path="/projects" element={<WorkInProgress />} /> 
+        <Route path="/blueprints" element={<WorkInProgress />} /> 
+        <Route path="/workflows" element={<WorkInProgress />} /> 
+        <Route path="/connection" element={<WorkInProgress />} /> 
+        <Route path="/generator" element={<WorkInProgress />} /> 
+        <Route path="/predictor" element={<WorkInProgress />} /> 
+        <Route path="/extender" element={<WorkInProgress />} /> 
+        <Route path="/apikeys" element={<WorkInProgress />} /> 
+        <Route path="/invites" element={<WorkInProgress />} /> 
+        <Route path="/usage" element={<WorkInProgress />} /> 
+        <Route path="/billings" element={<WorkInProgress />} /> 
+        <Route path="/settings" element={<WorkInProgress />} /> 
       </Route>
     </Routes>
   );
---
+++ src/pages/WorkInProgress.tsx
@@ -0,0 +1,9 @@
+
+
+export default function WorkInProgress() {
+    return (
+        <main className="flex-grow">
+            <h1>Work in progress</h1>
+        </main>
+    )
+}
