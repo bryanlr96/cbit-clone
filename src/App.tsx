a50dff2ba339303e62667bd32e4c245ed7106d69
Bryan <bryanlopezruiz1996@gmail.com>
2025-05-11 21:29:18 +0200

    cambiando path por index

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Routes>
       <Route path="/" element={<Layout />}> 
-        <Route path="/" element={<Playground />} /> 
+        <Route index element={<Playground />} /> 
       </Route>
     </Routes>
   );
